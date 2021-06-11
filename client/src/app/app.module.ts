e896c89366ca9e8050a62c0e6318f245940a7c3b
Yudan Zhai <yudanzhai@Yudans-MacBook-Pro.local>
2021-06-11 11:22:53 +0200

    adding register function

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    FormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  currentUser$ : Observable<User>;
+
+  constructor(private accountService: AccountService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  login() {
+    // console.log(this.model);
+    this.accountService.login(this.model).subscribe(response => {
+      console.log(response);
+
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  logout() {
+    this.accountService.logout();
+
+  }
+
+
+
+}
